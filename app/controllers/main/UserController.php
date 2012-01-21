4c2b54989328a101d93686e44f25c4270531f713
Bill Centinaro <Bill@Theresnobox.net>
2012-01-21 13:07:36 -0500

    Added the ability to control the URL a user is redirected to upon login.

--- app/controllers/main/UserController.php
+++ app/controllers/main/UserController.php
@@ -7,6 +7,8 @@ use hydrogen\log\Log;
 use vespora\models\sqlBeans\UserBean;
 use vespora\models\UserModel;
 use vespora\models\SessionModel;
+use vespora\helpers\sessionHelper;
+use vespora\helpers\viewHelper;
 
 
 class UserController extends BaseController {
@@ -17,8 +19,8 @@ class UserController extends BaseController {
      * @return null
      */
     public function index(){
-        //if ($this->session->isLoggedIn())
-            //return $this->profile();
+        if (sessionHelper::loggedIn())
+            return $this->profile();
         return $this->login();
 
     }
@@ -29,9 +31,12 @@ class UserController extends BaseController {
      * @return null
      */
     public function login(){
-        //if ($this->session->isLoggedIn())
-        //    return $this->redirect('/user/profile');
-        $GLOBALS['layout'] = 'user/login';
+        if (sessionHelper::loggedIn())
+            return $this->redirect('/user/profile');
+        View::setVar('returnURL', '/');
+        if(isset($_GET['return']))
+            View::setVar('returnURL', ($_GET['return']));
+        viewHelper::$layout = 'user/login';
         return null;
     }
 
@@ -39,37 +44,37 @@ class UserController extends BaseController {
      * gathers data posted back to the server, and attempts to authenticate against the provided credentials
      * should credentials be correct, it starts a session for the user and stores the ID in the session table
      *
-     * @return null
+     * @return null|void
      */
     public function login_post(){
-        $layout = 'user/login';
+
         if(isset ( $_POST ['username'] ) && $_POST ['password']){
             $userModel = UserModel::getInstance();
             if ($userModel->authenticate ( $_POST ['username'], $_POST ['password'] )) {
                 $user = $userModel->getUserByName ( $_POST ['username'] );
                 $this->createSession( $user->id );
                 View::setVar ( 'flashMessage', 'User Login Passed' );
-                $GLOBALS['layout'] = 'home/index';
-                //if ($this->session->isAdmin())
-                //    return $this->redirect('/admin');
+                viewHelper::$layout = 'home/index';
+                if($_POST['return'])
+                    return $this->redirect($_POST['return']);
                 return null;
             }
         }
         View::setVar ( 'flashMessage', 'User Login Failed' );
+        viewHelper::$layout = 'user/login';
         return null;
     }
 
     /**
      * displays the current user's profile
-     * @return null
      */
     public function profile(){
-        //if ($this->session->isLoggedIn()){
-        $GLOBALS['layout'] = 'user/profile';
-            return null;
-        //}
+        if (sessionHelper::loggedIn()){
+            viewHelper::$layout = 'user/profile';
+            return;
+        }
         $this->redirect('/user/login');
-        return null;
+        return;
     }
 
     /**
@@ -90,9 +95,9 @@ class UserController extends BaseController {
      * @param $userID
      */
     private function createSession($userID){
-        Log::info("Creating Session - User ID: $userID | Session ID: " . $this->session->sessionID);
-        SessionModel::getInstance()->insertSession($userID,   $this->session->sessionID);
-        $this->setupSession();
+        Log::info("Creating Session - User ID: $userID | Session ID: " . sessionHelper::$sessionID);
+        SessionModel::getInstance()->insertSession($userID,   sessionHelper::$sessionID);
+        sessionHelper::setupSession();
 
     }
 
@@ -102,7 +107,7 @@ class UserController extends BaseController {
      */
     private function destroySession(){
         $sessionModel = SessionModel::getInstance();
-        $sessionModel->deleteSession($this->session->sessionID);
+        $sessionModel->deleteSession(sessionHelper::$sessionID);
         return session_destroy();
     }
 }
