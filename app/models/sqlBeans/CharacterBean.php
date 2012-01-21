ac2924564ddcee0296ae831dcdcb77040a0eba92
Bill Centinaro <Bill@Theresnobox.net>
2012-01-21 13:06:32 -0500

    Added campaign_id and user_id to the character bean.

--- app/models/sqlBeans/CharacterBean.php
+++ app/models/sqlBeans/CharacterBean.php
@@ -2,6 +2,7 @@
 namespace vespora\models\sqlBeans;
 
 use hydrogen\sqlbeans\SQLBean;
+use vespora\models\CampaignModel;
 
 /**
  * User: bill
@@ -18,8 +19,32 @@ class CharacterBean extends SQLBean
     /**
      * An array of every field in the table.
      */
-    protected static $fields = array('id', 'name');
+    protected static $fields = array('id', 'name', 'campaign_id', 'user_id');
 
+    /**
+     * Gets the campaign bean assossiated with the current character bean.
+     * @return mixed
+     */
+    public function getCampaign(){
+        $campaignModel = CampaignModel::getInstance();
+        return $campaignModel->getCampaign($this->campaign_id);
+    }
+
+    /**
+     * A function to return the current bean data as an array.
+     *
+     * @param bool $full flag to fill out the bean, with all of it's linked content.
+     * @return array
+     */
+    public function toArray($full = false){
+        if(!$full)
+            return parent::toArray();
+
+        $beanData = $this->stored;
+        $beanData['campaign'] = $this->getCampaign();
+
+        return $beanData;
+    }
 }
 
 ?>
