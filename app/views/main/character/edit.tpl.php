62d818036e78981f5292e0214dd9f83fae924cd9
Bill Centinaro <Bill@Theresnobox.net>
2012-01-25 09:42:39 -0500

    Added stats to the edit display.

--- app/views/main/character/edit.tpl.php
+++ app/views/main/character/edit.tpl.php
@@ -4,4 +4,10 @@
 <h2>Edit: {{character.name}}</h2>
     <p>Campaign: {{character.campaign.name}} ({{character.type.name}})</p>
 
+<h3>Stats</h3>
+    <label>Agility: {{character.stats.Agility.value}}</label> <br />
+    <label>Smarts: {{character.stats.Smarts.value}}</label> <br />
+    <label>Spirit: {{character.stats.Spirit.value}}</label> <br />
+    <label>Strength: {{character.stats.Strength.value}}</label> <br />
+    <label>Vigor: {{character.stats.Vigor.value}}</label> <br />
 {% endblock %}
