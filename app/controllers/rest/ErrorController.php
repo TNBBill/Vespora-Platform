6e84006bd83c5f2052198f5d5927c6ff7e93919f
root <root@tnb-php-01.(none)>
2012-01-27 14:29:54 +0000

    Added some PHPDoc to the rest controllers, added last night.

--- app/controllers/rest/ErrorController.php
+++ app/controllers/rest/ErrorController.php
@@ -10,17 +10,25 @@ use vespora\helpers\sessionHelper;
 
 class ErrorController extends BaseController
 {
-
+    /**
+     * returns a JSON object with a 404 error. 
+     */
     public function notFound_get(){
         header("Status: 404 Not Found", true, 404);
         viewHelper::$layout = 'error/notFound';
     }
 
+    /**
+     * returns a JSON object with a 400 error, with the text 'Not supported call' 
+     */
     public function notSupported_get(){
         header("Status: 400 Bad Request", true, 400);
         viewHelper::$layout = 'error/notSupported';
     }
 
+    /**
+     * returns a JSON object with a 400 error, with the text 'expected value not found' 
+     */
     public function noValue_get(){
         header("Status: 400 Bad Request", true, 400);
         viewHelper::$layout = 'error/notValue';
