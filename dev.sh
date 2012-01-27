616303e9d2cfdf555ac5208a50a03bb02d337d6e
Bill Centinaro <Bill@TheresNoBox.net>
2012-01-27 17:42:30 +0000

    Modified deploy scripts, and git ignore.

--- dev.sh
+++ dev.sh
@@ -1,7 +1,12 @@
 rm -rf /projects/Vespora-Platform/webroot/files
 rm -rf /projects/Vespora-Platform/webroot/css
 rm -rf /projects/Vespora-Platform/webroot/js
-ln -s /projects/TNB-Clients/platform.vespora.com/webroot/files /projects/Vespora-Platform/webroot/files
+rm -rf /projects/Vespora-Platform/config/config.ini.php
+rm -rf /projects/Vespora-Platform/webroot/favicon.ico
+ln -s /projects/TNB-Clients/platform.vespora.com/files/files /projects/Vespora-Platform/webroot/files
 ln -s /projects/TNB-Clients/platform.vespora.com/compile/css /projects/Vespora-Platform/webroot/css
 ln -s /projects/TNB-Clients/platform.vespora.com/compile/js /projects/Vespora-Platform/webroot/js
+ln -s /projects/TNB-Clients/platform.vespora.com/dev.ini.php /projects/Vespora-Platform/config/config.ini.php
+ln -s /projects/TNB-Clients/platform.vespora.com/files/favicon.ico /projects/Vespora-Platform/webroot/favicon.ico
+touch /projects/Vespora-Platform/config/config.ini.php
 
--- prod.sh
+++ prod.sh
@@ -1,7 +1,12 @@
 rm -rf /projects/Vespora-Platform/webroot/files
 rm -rf /projects/Vespora-Platform/webroot/css
 rm -rf /projects/Vespora-Platform/webroot/js
-ln -s /projects/TNB-Clients/platform.vespora.com/webroot/files /projects/Vespora-Platform/webroot/files
-ln -s /projects/TNB-Clients/platform.vespora.com/webroot/css /projects/Vespora-Platform/webroot/css
-ln -s /projects/TNB-Clients/platform.vespora.com/webroot/js /projects/Vespora-Platform/webroot/js
+rm -rf /projects/Vespora-Platform/config/config.ini.php
+rm -rf /projects/Vespora-Platform/webroot/favicon.ico
+ln -s /projects/TNB-Clients/platform.vespora.com/files/files /projects/Vespora-Platform/webroot/files
+ln -s /projects/TNB-Clients/platform.vespora.com/files/css /projects/Vespora-Platform/webroot/css
+ln -s /projects/TNB-Clients/platform.vespora.com/files/js /projects/Vespora-Platform/webroot/js
+ln -s /projects/TNB-Clients/platform.vespora.com/prod.ini.php /projects/Vespora-Platform/config/config.ini.php
+ln -s /projects/TNB-Clients/platform.vespora.com/files/favicon.ico /projects/Vespora-Platform/webroot/favicon.ico
+touch /projects/Vespora-Platform/config/config.ini.php
 
