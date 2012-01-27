554fde02ae06be004698bf572e5bc05ce3fb514d
Bill Centinaro <Bill@Theresnobox.net>
2012-01-26 23:14:59 -0500

    added setup files for symlinks

---
+++ dev.sh
@@ -0,0 +1,7 @@
+rm -rf /projects/Vespora-Platform/webroot/files
+rm -rf /projects/Vespora-Platform/webroot/css
+rm -rf /projects/Vespora-Platform/webroot/js
+ln -s /projects/TNB-Clients/platform.vespora.com/webroot/files /projects/Vespora-Platform/webroot/files
+ln -s /projects/TNB-Clients/platform.vespora.com/compile/css /projects/Vespora-Platform/webroot/css
+ln -s /projects/TNB-Clients/platform.vespora.com/compile/js /projects/Vespora-Platform/webroot/js
+
---
+++ prod.sh
@@ -0,0 +1,7 @@
+rm -rf /projects/Vespora-Platform/webroot/files
+rm -rf /projects/Vespora-Platform/webroot/css
+rm -rf /projects/Vespora-Platform/webroot/js
+ln -s /projects/TNB-Clients/platform.vespora.com/webroot/files /projects/Vespora-Platform/webroot/files
+ln -s /projects/TNB-Clients/platform.vespora.com/webroot/css /projects/Vespora-Platform/webroot/css
+ln -s /projects/TNB-Clients/platform.vespora.com/webroot/js /projects/Vespora-Platform/webroot/js
+
