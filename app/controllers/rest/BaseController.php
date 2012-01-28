35f8000cf1fdcbd32c7315260fe3ef1e8029c05c
Bill Centinaro <Bill@Theresnobox.net>
2012-01-28 00:46:29 -0500

    Added parser for $_PUT data, to provide proper REST web service verb use.

--- app/controllers/rest/BaseController.php
+++ app/controllers/rest/BaseController.php
@@ -12,18 +12,34 @@ use vespora\helpers\viewHelper;
 
 
 class BaseController extends Controller {
+    protected $_PUT;
     /**
      * Rest base controller constructor, this function contains setup functions
      * for all of the rest controllers. It performs some basic security checks
      * sets the mime type to application/json then sets the theme to rest.
      *
      */
-    function __construct(){
+     function __construct(){
         parent::__construct();
+
+        switch($_SERVER['REQUEST_METHOD']){
+            case 'PUT':
+                // because PHP doesn't recognize the fact that PUT can contain data!
+                parse_str(file_get_contents("php://input"),$this->_PUT);
+            case 'POST':
+            case 'DELETE':
+
+            break;
+            case 'GET':
+
+        }
         if ($_SERVER['REQUEST_METHOD']!= 'GET'){
             //Security stuff here.
 
         }
+
+
+        parse_str(file_get_contents("php://input"),$post_vars);
         header('Content-type: application/json');
         viewHelper::$theme = 'rest';
     }
