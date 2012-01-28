b2f3b112f26533feef7b23d77e2843e8c7d87d01
Bill Centinaro <Bill@Theresnobox.net>
2012-01-28 00:47:56 -0500

    modified JSON object to mirror new data structure.

--- app/views/rest/character/Vespora/full.tpl.php
+++ app/views/rest/character/Vespora/full.tpl.php
@@ -4,13 +4,18 @@
     "type": "{{character.type.name}}",
     "stats":{
         {% for stat in character.stats %}
-        "{{stat.stat}}": "{{stat.value}}"
+        "{{stat.name}}": "{{stat.currentValue}}"
         {% endfor %}
         }
     "skills":{
-        {% for skill in character.skills %}
-       "{{skill.skill}}": "{{skill.value}}"
-        {% endfor %}
-        }
+            {% for skill in character.skills %}
+            "skill":{
+                "name": "{{skill.name}}",
+                "value": "{{skill.currentValue}}",
+                "attribute": "{{skill.stat}}"
+            }
+            {% endfor %}
+
+    }
 
 }
