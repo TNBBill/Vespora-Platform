de86da3ec65fdb68acd9e4a6d52ba5391471f8f3
Bill Centinaro <Bill@Theresnobox.net>
2012-01-28 16:00:12 -0500

    Added IDs to the labels.

--- app/views/main/character/Vespora/edit.tpl.php
+++ app/views/main/character/Vespora/edit.tpl.php
@@ -6,14 +6,15 @@
 
 <h3>Stats</h3>
     {% for stat in character.stats %}
-        <label>{{stat.name}}: {{stat.currentValue}}</label> <br />
+        <label id="Stat-{{stat.stat_id}}">{{stat.name}}: {{stat.currentValue}}</label> <br />
     {% endfor %}
 
 <h3>Skills</h3>
 {% for skill in character.skills %}
-<label>{{skill.name}} ({{skill.stat}}): {{skill.currentValue}}</label> <br />
+<label id='Skill-{{skill.skill_id}}'>{{skill.name}} ({{skill.stat}}): {{skill.currentValue}}</label> <br />
 {% endfor %}
 <label>General Knowledge: {{character.stats.Smarts.currentValue}}</label>
 
     <a href="#" onclick="updateStat('{{character.id}}','1', 'D8');" name="D8 Agility">D8 Agility</a>
+    <a href="#" onclick="updateStat('{{character.id}}','1', 'D8');" name="D8 Skill">D8 Agility</a>
 {% endblock %}
