428ffd0a51e42ba373b55bb960da44918d8f031d
Bill Centinaro <Bill@Theresnobox.net>
2012-01-28 14:58:31 -0500

    added images folder to the script.

--- prod.sh
+++ prod.sh
@@ -1,9 +1,11 @@
 rm -rf /projects/Vespora-Platform/webroot/files
+rm -rf /projects/Vespora-Platform/webroot/images
 rm -rf /projects/Vespora-Platform/webroot/css
 rm -rf /projects/Vespora-Platform/webroot/js
 rm -rf /projects/Vespora-Platform/config/config.ini.php
 rm -rf /projects/Vespora-Platform/webroot/favicon.ico
 ln -s /projects/TNB-Clients/platform.vespora.com/files/files /projects/Vespora-Platform/webroot/files
+ln -s /projects/TNB-Clients/platform.vespora.com/files/images /projects/Vespora-Platform/webroot/images
 ln -s /projects/TNB-Clients/platform.vespora.com/files/css /projects/Vespora-Platform/webroot/css
 ln -s /projects/TNB-Clients/platform.vespora.com/files/js /projects/Vespora-Platform/webroot/js
 ln -s /projects/TNB-Clients/platform.vespora.com/prod.ini.php /projects/Vespora-Platform/config/config.ini.php
