f4c6986ad1d35645e5ab6643321f3ba858c83db7
Bill Centinaro <Bill@Theresnobox.net>
2012-01-29 11:01:10 -0500

    Added pull and push scripts.

---
+++ pull.sh
@@ -0,0 +1,6 @@
+cd /projects/TNB-Clients
+git pull
+cd /projects/Vespora-Platform
+git pull
+cd /projects/Vespora-Platform/lib/Hydrogen
+git pull
---
+++ push.sh
@@ -0,0 +1,13 @@
+cd /projects/TNB-Clients
+./wro.sh platform.vespora.com
+git add .
+git commmit
+git push
+cd /projects/Vespora-Platform
+git add .
+git commmit
+git push
+cd /projects/Vespora-Platform/lib/Hydrogen
+git add .
+git commmit
+git push
