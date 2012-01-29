691777025ec8d0d9f3b38c7fd15c81af9c002b28
Bill Centinaro <Bill@Theresnobox.net>
2012-01-29 11:03:15 -0500

    Updated push script

---
+++ database/Data.sql
@@ -0,0 +1,8 @@
+-- Inserting user permissions
+
+INSERT INTO `VesporaPlatform`.`vespora_user_permission`
+(`rolename`, `superadmin`,`admin`)
+VALUES
+('Guest',0,0),
+('User',0,0),
+('Admin',0,1);
--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 cd /projects/TNB-Clients
 ./wro.sh platform.vespora.com
 git add .
-git commmit
+git commit
 git push
 cd /projects/Vespora-Platform
 git add .
-git commmit
+git commit
 git push
 cd /projects/Vespora-Platform/lib/Hydrogen
 git add .
-git commmit
+git commit
 git push
